913ae35af524b9026d70f1e9f83b7b5a5697719b
Corbin Petzinger <corbin.petzinger@gmail.com>
2025-09-25 13:30:23 +0000

    oh boy

---
+++ src/com/anger/Main.java
@@ -0,0 +1,8 @@
+package com.anger;
+
+public class Main {
+
+
+
+    
+}
---
+++ src/com/anger/SignInSheet.java
@@ -0,0 +1,30 @@
+package com.anger;
+
+public class SignInSheet {
+
+    public String name;
+    public int onTime;
+
+    public SignInSheet(String name) {
+    this.name = name;
+    }
+
+    public SignInSheet(int Time) {
+        this.onTime = onTime;
+    }
+
+    public void getname(){
+        System.out.println("your name is " + name);
+        
+    public void getSignInSheet(){
+         System.out.println("you have arrived at");
+    }
+       
+    }
+
+
+
+
+
+
+}
